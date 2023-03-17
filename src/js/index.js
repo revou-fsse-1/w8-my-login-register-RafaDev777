90025dee44dc38d99db0f3234647d2adf347cf72
Rafael Jonathan <rafaeljonathan.work@gmail.com>
2023-03-17 23:39:38 +0700

    complete SignUp function

--- src/js/index.js
+++ src/js/index.js
@@ -7,24 +7,24 @@ const checkStringLength = (input) => {
 };
 
 const checkEmail = (input) => {
-  return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input);
+  return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g.test(input);
 };
 
 const checkUpperCase = (input) => {
-  return /[A-Z]/.test(input);
+  return /[A-Z]/g.test(input);
 };
 
 const checkLowerCase = (input) => {
-  return /[a-z]/.test(input);
+  return /[a-z]/g.test(input);
 };
 
 const checkNumber = (input) => {
-  return /[0-9]/.test(input);
+  return /[0-9]/g.test(input);
 };
 
 const getData = localStorage.getItem("userData");
 const userData = JSON.parse(localStorage.getItem("userData")) || [
-  { email: "admin@gmail.com", password: "Adm123123" },
+  { id: 1, email: "admin@gmail.com", password: "Adm123123" },
 ];
 localStorage.setItem("userData", JSON.stringify(userData));
 
@@ -35,8 +35,6 @@ const signIn = () => {
   const checkCredential = userData.find(
     (a) => a.email === email && a.password === pw
   );
-  console.log(userData);
-  console.log(checkCredential);
 
   switch (false) {
     case checkEmptyString(email):
@@ -48,24 +46,54 @@ const signIn = () => {
     case checkEmptyString(pw):
       console.log("Please fill in your password!");
       break;
-    // case checkStringLength(pw):
-    //   console.log("Your password must contain at least 8 characters!");
-    //   break;
-    // case checkUpperCase(pw):
-    //   console.log("Your password must contain at least 1 uppercase!");
-    //   break;
-    // case checkLowerCase(pw):
-    //   console.log("Your password must contain at least 1 lowercase!");
-    //   break;
-    // case checkNumber(pw):
-    //   console.log("Your password must contain at least 1 number!");
-    // break;
     case checkCredential !== undefined:
-      console.log("Please check your password!");
+      console.log("You may enter wrong emali or password.");
       break;
     case checkCredential === undefined:
       console.log("success");
       window.location.href = "./pages/home/";
+      break;
+  }
+};
+
+const signUp = () => {
+  let email = document.querySelector("#emailReg").value;
+  let pw = document.querySelector("#pwReg").value;
+
+  const checkDuplicate = userData.find((a) => a.email === email);
+
+  switch (false) {
+    case checkEmptyString(email):
+      console.log("Please fill in your email!");
+      break;
+    case checkEmail(email):
+      console.log("Please enter a valid email!");
+      break;
+    case checkEmptyString(pw):
+      console.log("Please fill in your password!");
+      break;
+    case checkStringLength(pw):
+      console.log("Your password must contain at least 8 characters!");
+      break;
+    case checkUpperCase(pw):
+      console.log("Your password must contain at least 1 uppercase!");
+      break;
+    case checkLowerCase(pw):
+      console.log("Your password must contain at least 1 lowercase!");
+      break;
+    case checkNumber(pw):
+      console.log("Your password must contain at least 1 number!");
+      break;
+    case checkDuplicate === undefined:
+      console.log("This email has been registered");
+      break;
+    case checkDuplicate !== undefined:
+      userData.push({
+        id: userData[userData.length - 1].id + 1,
+        email: email,
+        password: pw,
+      });
+      localStorage.setItem("userData", JSON.stringify(userData));
       break;
   }
 };
